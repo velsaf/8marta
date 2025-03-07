2178809e6f5073d81b054e94b759fd8d4c52026a
velsaf <gdxacc64@gmail.com>
2025-03-07 23:38:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function startHearts() {
         heart.style.left = Math.random() * 100 + 'vw';
         heart.style.top = Math.random() * -20 + 'vh';
         heart.style.animationDuration = Math.random() * 2 + 3 + 's';
-        heart.style.fontSize = Math.random() * 20 + 10 + 'px';
+        heart.style.fontSize = Math.random() * 15 + 10 + 'px';
 
         heartsContainer.appendChild(heart);
 
